37117dd2215481a8d390a72240847309be54457a
KT 2022 <103476893+kt946@users.noreply.github.com>
2022-05-28 16:41:00 -0400

    sorted list by highest score

--- assets/js/score.js
+++ assets/js/score.js
@@ -1,15 +1,21 @@
-var scoreList = document.getElementById("score-list");
-
 var loadList = function() {
     // if list exists, get list; otherwise set list to empty
     var userList = JSON.parse(localStorage.getItem("user")) || [];
+
+    // sort users based on highest score
+    userList.sort(function (a , b){
+        return b.score - a.score
+    });
+
+    // load list of scores
     for (i = 0; i < userList.length; i++) {
         var listEl = document.createElement("li");
         listEl.textContent = userList[i].initials + " - " + userList[i].score;
-        scoreList.append(listEl);
+        document.getElementById("score-list").appendChild(listEl);
     }
 };
 
+// clear high scores and reload page
 var clearScore = function() {
     localStorage.removeItem("user");
     location.reload(); 
